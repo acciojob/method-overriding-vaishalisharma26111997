2e55698044c65ef756c733be68be35d70cf3a477
vaishali-sharma <iitmgwl1975@gmail.com>
2023-03-26 20:49:39 +0530

    [task completed]

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,23 @@ package com.driver;
 
 public class Main {
 
+    public static class A {
+        public String meth() {
+            return "Invoking method from class A";
+
+        }
+    }
+
+    public static class B extends A {
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+
+        }
+    }
+
+    public static void main(String[] args) {
+        B obj = new B();
+        System.out.println(obj.meth());
+    }
+
 }
