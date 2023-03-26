7f2ca013c18c519dec3d0ef7fdd6e7c703d6dc25
vaishali-sharma <iitmgwl1975@gmail.com>
2023-03-26 15:59:25 +0530

    [task -3 completed]

---
+++ src/main/java/A.java
@@ -0,0 +1,17 @@
+
+public class A {
+    public String meth() {
+        return "Invoking method from class A";
+
+    }
+}
+class B extends A{
+   // public String meth() {
+   //     return "Method is overridden in Extendend class B";
+
+   // }
+    public static void main (String [] args) {
+        B obj = new B();
+        System.out.println(obj.meth());
+    }
+}
